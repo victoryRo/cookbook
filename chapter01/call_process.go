f9fa2fe918ada6b03c1c28e2c5883e3c86b06651
victor <victoriess77@gmail.com>
2023-09-21 10:37:39 -0500

    we create comments explaining the methods

--- chapter01/call_process.go
+++ chapter01/call_process.go
@@ -8,15 +8,19 @@ import (
 
 // CallProcessExternal ...
 func CallProcessExternal() {
+	// ejecuta comando en la terminal desde go
 	prc := exec.Command("ls", "-a")
 	out := bytes.NewBuffer([]byte{})
 
 	prc.Stdout = out
+	// inicia el cmd y espera hasta que se ejecute
 	err := prc.Run()
 	if err != nil {
 		panic(err)
 	}
 
+	// prc.ProcessState continer info acerca de la salida del process
+	// prc.ProcessState.Success() reporta si la salida del process fue exitosa
 	if prc.ProcessState.Success() {
 		fmt.Println("Process run successfully with output:")
 		fmt.Println(out.String())
@@ -29,11 +33,13 @@ func CallProcess2() {
 	out := bytes.NewBuffer([]byte{})
 
 	prc.Stdout = out
+	// inicia el cmd especificado pero no espera hasta que se complete
 	err := prc.Start()
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	// espera hasta que la salida del cmd se complete
 	prc.Wait()
 
 	if prc.ProcessState.Success() {
