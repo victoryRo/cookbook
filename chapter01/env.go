f8d2056c1b7d93173b60779882f596210c914525
victor <victoriess77@gmail.com>
2023-10-02 17:55:54 -0500

    ENV: we set environment variables and check them

--- chapter01/env.go
+++ chapter01/env.go
@@ -6,16 +6,21 @@ import (
 	"os"
 )
 
-// EnvVariables ...
+// EnvVariables ... study
 func EnvVariables() {
+	// recupera el valor de la env
+	// pero no comprueba si esta vacio o no ha sido establecida
 	connStr := os.Getenv("DB_CONN")
-	log.Printf("Connection string: %s", connStr)
+	l := len(connStr)
+	log.Printf("Connection string: %s len: %d", connStr, l)
 }
 
 // EnvNoSet ...
 func EnvNoSet() {
 	key := "DB_CONN"
+	// os.Setenv(key, "Whife")
 
+	// recupera el valor de la env, si esta vacia retorna false
 	connStr, ok := os.LookupEnv(key)
 	if !ok {
 		log.Printf("The env variable %s is not set.\n", connStr)
@@ -27,22 +32,25 @@ func EnvNoSet() {
 func EnvVarTest() {
 	key := "DB_CONN"
 
-	//set the environment variable
-	err := os.Setenv(key, "postgres://as:as@example.com/pg?sslmode=verify-full")
+	// le asignamos el valor a la env
+	err := os.Setenv(key, "first_environment_variable")
 	if err != nil {
 		log.Fatal(err)
 	}
-	val := getEnvDefault(key, "postgres://as:as@localhost/pg?sslmode=verify-full")
-	log.Println("the value is:" + val)
+	val := getEnvDefault(key, "send_var_by_default")
+	log.Println("the value is: " + val)
 
+	// desabilita un variable de entorno
 	err = os.Unsetenv(key)
 	if err != nil {
 		log.Printf("Error the UnsetEnv function %v", err)
 	}
-	val = getEnvDefault(key, "postgres://as:as@127.0.0.1/pg?sslmode=verify-full")
-	log.Println("the default value is:" + val)
+	val = getEnvDefault(key, "second_var_by_default")
+	log.Println("the default value is :" + val)
 }
 
+// getEnvDefault ...
+// comprueba si exite la env y la retorna, si no le asignamos una por default
 func getEnvDefault(key, defVal string) string {
 	val, ok := os.LookupEnv(key)
 	if !ok {
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	chapter01.Parameterize()
+	chapter01.EnvVarTest()
 }
